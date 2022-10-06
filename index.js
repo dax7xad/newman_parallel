848307ee5c15a1390af631dc4fc0c113252519d4
Alvaro Dax Diaz Amaya <dax7xad@gmail.com>
2022-10-06 17:29:19 -0600

    agregando los parametros del htmlextra

--- index.js
+++ index.js
@@ -15,7 +15,34 @@ const parametersForTestRun = {
   ), //your env
   reporters: ["htmlextra"],
   reporter: {
-    htmlextra: { export: `log/responses/${date}/response.html` },
+    htmlextra: {
+      export: `log/responses/${date}/response.html`,
+       template: './postman/template.hbs',
+       logs: true,
+      // showOnlyFails: true,
+      // noSyntaxHighlighting: true,
+      // testPaging: true,
+      // browserTitle: "My Newman report",
+      // title: "My Newman Report",
+      // titleSize: 4,
+      // omitHeaders: true,
+      // skipHeaders: "Authorization",
+      // omitRequestBodies: true,
+      // omitResponseBodies: true,
+      // hideRequestBody: ["Login"],
+      // hideResponseBody: ["Auth Request"],
+      // showEnvironmentData: true,
+      // skipEnvironmentVars: ["API_KEY"],
+      // showGlobalData: true,
+      // skipGlobalVars: ["API_TOKEN"],
+      // skipSensitiveData: true,
+      // showMarkdownLinks: true,
+      // showFolderDescription: true,
+      // timezone: "Australia/Sydney",
+      // skipFolders: "folder name with space,folderWithoutSpace",
+      // skipRequests: "request name with space,requestNameWithoutSpace",
+      // displayProgressBar: true
+    },
   },
 };
 
